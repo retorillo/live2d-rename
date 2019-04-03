7c1671b2ab2024ad27ad5a49d4cca533bd2e195d
Retorillo <retorillo@outlook.com>
2019-04-03 16:40:46 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# live2d-rename
+
+`live2d-rename` command can change name and ID of Live2D avator directory and content to successfully read by FaceRig.
+
+- Change directory name
+- Change MODEL3.JSON file and all files refered by it (eg. PHYSICS3.JSON, texture directories)
+- Change CFG file names and its IDs (ID must be matched with CFG file name)
+- Change Icon file name
+
+Tested on Cubism 3.2 format files + [FaceRig (Steam)](https://store.steampowered.com/app/274920/FaceRig/)
+
+## Installation
+
+```
+npm install --global https://github.com/retorillo/live2d-rename.git
+```
+
+OR
+
+```
+git clone https://github.com/retorillo/live2d-rename.git
+cd live2d-rename
+npm link
+```
+
+## Usage
+
+```
+live2d-rename --src .\foo --dist .\bar
+```
+
+- `--src` (or `-s`, `--source`) [Mandatory] Specify source directory 
+- `--dest` (or `-d`, `--destination) [Mandatory] Specify destinatoin directory (new name)
+- `--force` (or `-f`) [Optional] Skip confirmation to remove destination directory during operation
+- `--no-dupl` (or `-N`, `-nd`, `--no-duplication`) [Optional] Source directory will be removed after renamed (NOT RECOMMENDED BEFORE TEST)
+- `--install` (or `-I`) [Optional, Experimantal] Install destination content to FaceRig
+
+## License
+
+MIT
+
+(C) 2019 Retorillo
---
+++ bin/cli.js
@@ -0,0 +1,207 @@
+#!/usr/bin/env node
+
+const fs = require('fs').promises;
+const os = require('os');
+const deepkey = require('deep-key');
+const deleteEmpty = require('delete-empty');
+const glob = require('glob');
+const iconv = require('iconv-lite');
+const path = require('path');
+const regexEscape = require('escape-string-regexp');
+const chardet = require('chardet');
+const copydir = require('copy-dir');
+const exists = require('path-exists');
+const del = require('del');
+const gnuopt = require('gnu-option');
+const optmap = {
+  's': '&src',
+  'src': '&source',
+  'source': 'string',  
+  'd': '&dest',
+  'dest': '&destination',
+  'destination': 'string',
+  'N': '&no-dupl',
+  'no-dupl': '&no-duplication',
+  'no-duplication': 'switch',
+  'f': '&force',
+  'force': 'switch',
+  'I': '&install',
+  'install': 'switch',
+};
+const options = gnuopt.parse(optmap);
+
+async function prompt(msg, choices) {
+  var choice = -1;
+  choices = choices.toLowerCase().split(/(?=.)/);
+  process.stdin.setRawMode(true);
+  process.stdin.resume();
+  process.stdout.write(`${msg} [${choices.join('/')}]: `);
+  var awaiter_callback = (resolve) => {
+    if (choice != -1) resolve(); 
+    else setTimeout(awaiter_callback.bind(this, resolve), 50);
+  }
+  var awaiter = new Promise(awaiter_callback);
+  // TODO: BUG: 'data' event may be registered in duplication when called repatedly
+  process.stdin.on('data', chunk => {
+    if (choice != -1) return;
+    choice = choices.indexOf(chunk.toString().toLowerCase());
+    console.log(chunk.toString());
+  });
+  await awaiter;
+  process.stdin.pause();
+  return choices[choice];
+}
+function detectCharCode(file) {
+  return new Promise((resolve, reject) => {
+    chardet.detectFileAll(file, (err, result) => {
+      if (err) reject(err);
+      else resolve(result);
+    });
+  });
+}
+async function handleCfg(dir, before, after, prefix) {
+  const before_cfg = path.join(dir, `${prefix}${before}.cfg`);
+  const after_cfg = path.join(dir, `${prefix}${after}.cfg`);
+  if (!await exists(before_cfg)) {
+    console.log(`[WARNING] ${prefix}${before}.cfg is not found, operation is skipped`);
+    return;
+  }
+  const fenc = (await detectCharCode(before_cfg))[0];
+  await fs.rename(before_cfg, after_cfg);
+  console.log(`${before}.cfg is renamed to ${after}.cfg`);
+  var data = await fs.readFile(after_cfg);
+  data = iconv.decode(data, fenc.name);
+  var dos = /\r\n/.test(data);
+  console.log(`${after}.cfg is opened in ${fenc.name} (confidence: ${fenc.confidence}%)`);
+  var replaced = 0;
+  data = data.split(/\r?\n/).map(l => {
+    const regex = new RegExp(`([0-9a-z_]+\\s+)${regexEscape(before)}`, 'i');
+    if (!regex.test(l)) return l;
+    replaced++;
+    return l.replace(regex, `$1${after}`);
+  }).join(dos ? '\r\n' : '\n');
+  await fs.writeFile(after_cfg, data, { encoding: 'utf-8' });
+  console.log(`${after}.cfg is replaced (${replaced} lines)`);
+}
+async function handleJson(dir, before, after, ext) {
+  var affectedItems = [];
+  const before_cfg = path.format({ dir: dir, base: before + ext });
+  const after_cfg = path.format({ dir: dir, base: after + ext } );
+  if (!await exists(before_cfg)) {
+    console.log(`[WARNING] ${before}${ext} is not found. operation is skipped`);
+    return affectedItems;
+  }
+  const fenc = (await detectCharCode(before_cfg))[0];
+  await fs.rename(before_cfg, after_cfg);
+  console.log(`${before}${ext} is renamed to ${after}${ext}`);
+  var data = await fs.readFile(after_cfg);
+  data = iconv.decode(data, fenc.name);
+  console.log(`${after}${ext} is opened in ${fenc.name} (confidence: ${fenc.confidence}%)`);
+  var count = 0;
+  var json = JSON.parse(data);
+  var regex = new RegExp(regexEscape(before) + '(\\.)');
+  var replace = (value) => {
+    if (!regex.test(value)) return value;
+    var replaced = value.replace(regex, `${after}$1`);
+    affectedItems.push({ before: value, after: replaced });
+    count++;
+    return replaced;
+  }
+  for (dkey of deepkey.keys(json)) {
+    var value = deepkey.get(json, dkey);
+    if (typeof(value) === 'string')
+      deepkey.set(json, dkey, replace(value));
+  }
+  await fs.writeFile(after_cfg, JSON.stringify(json, null, 2), { encoding: 'utf-8' });
+  console.log(`${after}${ext} is replaced (${count} string objects)`);
+  return affectedItems;
+}
+async function handleIcon(dir, before, after) {
+  var beforepath = path.join(dir, `ico_${before}.png`);
+  var afterpath = path.join(dir, `ico_${after}.png`);
+  if (!await exists(beforepath)) { 
+    console.log(`[WARNING] ico_${before}.png is not found, operation is skipped`);
+    return;
+  }
+  await fs.rename(beforepath, afterpath);
+  console.log(`ico_${before}.png is renamed to ico_${after}.png`);
+}
+async function move(dir, before, after) {
+  var bpath = path.join(dir, before);
+  var apath = path.join(dir, after);
+  await fs.mkdir(path.dirname(apath), { recursive: true });
+  await fs.rename(bpath, apath);
+  console.log(`${before} is renamed to ${after}`);
+}
+async function exec() {
+  var src = options.source;
+  var dest = options.destination;
+  var force = options.force;
+  var nodupl = options['no-duplication'];
+  var inst = options.install;
+
+  if (!src)
+    throw '--src option is mandatory';
+  if (!dest)
+    throw '--dest option is mandatory';
+
+  if (!await exists(src))
+    throw `Path not found: ${src}`; 
+  var srcstat = await fs.stat(src)
+  if (!srcstat.isDirectory())
+    throw `Is not directory: ${src}`;
+  if (await exists(dest)) {
+    var c = force ? 'y' : await prompt(`${dest} already exists, remove it and continue operation?`, 'yn');
+    if (c == 'y') {
+      await del(dest);
+      console.log(`${dest} is deleted`);
+    }
+    else
+      throw 'Operation is canceled';
+  }
+  console.log(`copying ${src} to ${dest}`);
+  copydir.sync(src, dest);
+
+  const before = path.basename(src);
+  const after = path.basename(dest);
+
+  var affectedItems = await handleJson(dest, before, after, '.model3.json');
+  for (var i of affectedItems)
+    await move(dest, i.before, i.after);
+
+  await handleCfg(dest, before, after, 'cc_');
+  await handleCfg(dest, before, after, 'cc_names_');
+  await handleIcon(dest, before, after);
+  await deleteEmpty(dest);
+
+  if (nodupl) {
+    await del(src);
+    console.log(`${src} is deleted`);
+  }
+  if (inst) {
+    await install(dest, force);
+  }
+}
+async function install(dir, force) {
+  if (os.platform() !== 'win32') {
+    console.log('[WARNING] --install switch is avaiable only on Windows, operation is skipped');
+    return; 
+  }
+  var pdir = process.env[os.arch() == 'x64' ? 'ProgramFiles(x86)' : 'ProgramFiles'];
+  pdir = path.join(pdir, 'Steam\\steamapps\\common\\FaceRig\\Mod\\VP\\PC_CustomData\\Objects');
+  if (!await exists(pdir)) {
+    console.log('[WARNING] FaceRig install directory is not found, operation is skipped')
+    return;
+  }
+  var dest = path.join(pdir, path.basename(dir));
+  if (await exists(pdir)) {
+    var p = force ? 'y' : await prompt(`${dest} already exists, remove it and continue operation?`, 'yn')
+    if (p == 'n') {
+      console.log('[WARNING] Install operation is canceled by user');
+      return;
+    }
+  }
+  copydir.sync(dir, dest);
+  console.log(`installation is completed:\n=> ${dest}`);
+}
+exec();
